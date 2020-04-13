7160ed911c0ce2f4a639bb99710a1c6a8fa264eb
Negri48 <negri.victor.48@example.com>
2020-04-13 13:37:47 +0200

    Practica 3

--- PR3/pract3a.asm
+++ PR3/pract3a.asm
@@ -1,23 +1,37 @@
 ;**************************************************************************
 ;* BASIC ASSEMBLY PROGRAM STRUCTURE EXAMPLE to use with C code link
 ;* SBM / MBS 2020
-;*
+;* Autores: Rodrigo Lardiés Guillén
+;           Víctor Sánchez de la Roda Núñez
 ;**************************************************************************
-
+ 
 
 ; CODE SEGMENT DEFINITION
 _TEXT SEGMENT BYTE PUBLIC 'CODE'
-ASSUME CS: _TEXT, 
+ASSUME CS: _TEXT,
 
 
-PUBLIC _calculaMediana  ; example for the first function
+;********************************************************************************************************************************************************************
+;; Look Up Tables para el tercer apartado
+tabla db 84,82,87,65,71,77,89,70,80,68,88,66,78,74,90,83,81,86,72,76,67,75,69 ;Tabla que contiene valores ASCII de las letras de los DNI
+coef_mod_23 db 14,6,19,18,11,8,10,1 ;Tabla que contiene los coeficientes que acompañan a cada cifra en el algoritmo modular del apartado 3 (explicado después)
+;*********************************************************************************************************************************************************************
+
+
+
+
+
+;*********************************************************************************************************************************************************************
+; funcion1: _calculaMediana
+
+PUBLIC _calculaMediana 
 _calculaMediana PROC FAR 
-
+;Guardamos el puntero a la pila
 PUSH BP
 MOV BP, SP
-; Push the registered used 
-
-; Recover the data from stack , BP+6, etc (integers, pointers, etc)
+;Hacemos push a la pila los registros utilizados
+PUSH BX DX CX DI
+;Recuperamos las variables necesarias para el ejercicio
 LES BX,[BP +14]
 MOV DX,[BP + 6]
 MOV CX,[BP + 8]
@@ -54,77 +68,151 @@ SAR AX,1
 ; return the result in AX if needed.
 
 ; pop the used registers
-
-POP BP	;BP restored
+POP DI CX DX BX BP	;BP restored
 RET
 
 _calculaMediana ENDP
+;*************************************************************************************************************
 
 
 
+
+;*************************************************************************************************************
+; funcion2: _enteroACadenaHexa
 PUBLIC _enteroACadenaHexa  ; example for the first function
 _enteroACadenaHexa PROC FAR 
 PUSH BP
 MOV BP, SP
 
 PUSH AX
+PUSH DI
+PUSH CX
+PUSH DX
 
-PUSH BX
 
+
+LES DI,[BP + 8]
 MOV AX,[BP + 6]
+MOV CX, 0 ;;Contador
+MOV WORD PTR ES:[DI],0
+MOV WORD PTR ES:[DI+2],0
 
-MOV BX,[BP + 8]
-LES CX,[BP + 10]
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+CMP AX,0
+JZ CASO_CERO
 
-BUCLE: CMP AX, 0
-JZ FIN
-MOV DX,0
-MOV DI, 10H
-
-IDIV DI
-
-CMP DX,0AH
-JS NUMERO
-
-ADD DX,37H
-JMP LETRA
-NUMERO: ADD DX,30H
-LETRA:
-MOV DS:[BX], DL
-ADD BX,1
+BUCLE: 
+    CMP AX, 0
+    JZ BUCLE_PILA
+    MOV DX,0
+    MOV BX, 10H
+    IDIV BX
+    CMP DX,0AH
+    JS NUMERO
+    ADD DX,37H
+    JMP LETRA
+    NUMERO: 
+        ADD DX,30H
+    LETRA:
+        PUSH DX
+    INC CX
 JMP BUCLE
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-FIN:
 
-MOV DS:[BX], 00H
 
-POP BX
+CASO_CERO:
+    MOV ES:[DI],30H
+    INC DI 
+    JMP TERMINAR
+
+
+BUCLE_PILA:
+    CMP CX,0
+    JZ TERMINAR
+    POP AX
+    MOV ES:[DI],AL
+    INC DI
+    DEC CX
+    JMP BUCLE_PILA
+
+TERMINAR:
+
+
+MOV ES:[DI], 00H
+POP CX
+POP DX
+POP DI
 POP AX
 POP BP	;BP restored
 RET
 _enteroACadenaHexa ENDP
+;*************************************************************************************************************
 
 
 
-
+;*************************************************************************************************************
+; funcion3: _calculaLetraDNI
 PUBLIC _calculaLetraDNI  ; example for the first function
 _calculaLetraDNI PROC FAR 
 
 
-PUSH BP
+PUSH BP 
 MOV BP, SP
 
-POP BP	;BP restored
+PUSH SI DI CX DX AX BX ES DS
+
+LDS DI,[BP + 6] ;;Nº DNI
 
 
+MOV CX, 8 ;;CONTADOR
 
+;;AX = BYTE DE DNI / RESULTADO
+;; SI = OFFSET
+;;  CX  = CONTADOR
+;;DX = 
+;; BX = SUMA
+MOV SI, OFFSET coef_mod_23
+MOV DX,0 
+MOV BX,0
+BUCLE_DNI:
+    MOV AX,0
+    CMP CX,0
+    JZ TERMINAR_DNI
+    MOV DL, byte ptr CS:[SI]
+    MOV AL, byte ptr DS:[DI]
+    SUB AX,30H
+    MUL DL
+    ADD BX,AX
+    INC SI
+    INC DI
+    DEC CX
+JMP BUCLE_DNI
+
+
+TERMINAR_DNI:
+;;En BX tenemos el resultado de la aritmética modular del nº del DNI
+;;Queremos ahora dividirlo por 23 y quedarnos con el resto
+MOV AX,BX
+MOV BX,0
+MOV BX,23
+DIV BL
+
+;;El resto está en AH, queremos mirar en LUT
+MOV DX,0
+MOV DL,AH
+MOV BX,0
+;;Obtenemos la dirección donde almacenaremos la letra
+LES SI,[BP + 10]
+MOV BX, OFFSET tabla 
+ADD BX, DX
+
+MOV DL, byte ptr CS:[BX]
+MOV byte ptr ES:[SI], DL
+POP DS ES BX AX DX CX DI SI BP	;BP restored
 RET
 _calculaLetraDNI ENDP
-
+;*************************************************************************************************************
 _TEXT ENDS
-
 END
